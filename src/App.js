73a0d2819e5e6afbe97ce557e7bb1a0d486918ae
Kashif Ali <kashifali.bscssef20@iba-suk.edu.pk>
2023-11-22 19:17:16 +0500

    UI is added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAnalyzeClick = () => {};
+
+  const handleGenerateClick = () => {};
+
+  return (
+    <>
+      <h1>Computer Vision</h1>
+      <label htmlFor='input-field'>Insert URL or type prompt:</label>
+      <br />
+      <input
+        type='text'
+        id='input-field'
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder='Enter URL to analyze or textual prompt to generate image'
+      />
+      <br />
+      <button onClick={handleAnalyzeClick}>Analyze</button>
+      <button onClick={handleGenerateClick}>Generate</button>
+    </>
+  );
 }
 
 export default App;
